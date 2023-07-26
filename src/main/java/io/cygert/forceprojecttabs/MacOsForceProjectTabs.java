9065780dabf3ba73c882deef221aeacee6c1ed97
Mateusz Cygert <matcyg@gmail.com>
2023-07-26 21:16:45 +0200

    Update minimum required IntelliJ version to 2023.1. Fix deprecated API usage.

--- src/main/java/io/cygert/forceprojecttabs/MacOsForceProjectTabs.java
+++ src/main/java/io/cygert/forceprojecttabs/MacOsForceProjectTabs.java
@@ -6,22 +6,28 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.openapi.wm.WindowManager;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static com.intellij.openapi.actionSystem.PlatformCoreDataKeys.CONTEXT_COMPONENT;
 
-class MacOsForceProjectTabs implements StartupActivity.Background {
+class MacOsForceProjectTabs implements ProjectActivity {
+
+    @Nullable
     @Override
-    public void runActivity(@NotNull Project project) {
+    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         var mergeAllWindowsAction = ActionManager.getInstance().getAction("MergeAllWindowsAction");
         if(mergeAllWindowsAction == null) {
-            return;
+            return null;
         }
         var firstProject = getFirstProjectOrCurrent(project);
         var fakeEvent = createFakeActionEvent(firstProject);
         mergeAllWindowsAction.actionPerformed(fakeEvent);
+        return null;
     }
 
     private Project getFirstProjectOrCurrent(Project project) {
