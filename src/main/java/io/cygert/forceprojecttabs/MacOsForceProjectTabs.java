e4f8325136b14f7a25cc7bd7c3a9a4fdf5b03f7f
Mateusz Cygert <matcyg@gmail.com>
2025-03-24 19:01:11 +0100

    Upgrade to IntelliJ 2024.3.5 due to API deprecation
    - Upgrade gradle plugin to 2.4.0
    - Change minimal supported version to 2024.3.5
    - Remove deprecated API usage
    - Run MergeAllWindowsAction on EDT thread

--- src/main/java/io/cygert/forceprojecttabs/MacOsForceProjectTabs.java
+++ src/main/java/io/cygert/forceprojecttabs/MacOsForceProjectTabs.java
@@ -1,33 +1,52 @@
 package io.cygert.forceprojecttabs;
 
+import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionUiKind;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ex.ActionUtil;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.mac.foundation.Foundation;
+import com.intellij.ui.mac.foundation.MacUtil;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import static com.intellij.openapi.actionSystem.PlatformCoreDataKeys.CONTEXT_COMPONENT;
 
 class MacOsForceProjectTabs implements ProjectActivity {
 
     @Nullable
     @Override
     public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
-        var mergeAllWindowsAction = ActionManager.getInstance().getAction("MergeAllWindowsAction");
-        if(mergeAllWindowsAction == null) {
+        if (!isIdeFullyInitialized(project)) {
             return null;
         }
-        var firstProject = getFirstProjectOrCurrent(project);
-        var fakeEvent = createFakeActionEvent(firstProject);
-        mergeAllWindowsAction.actionPerformed(fakeEvent);
+
+        ApplicationManager.getApplication().invokeLater(() -> {
+            var mergeAllWindowsAction = ActionManager.getInstance().getAction("MergeAllWindowsAction");
+            if (mergeAllWindowsAction == null) {
+                return;
+            }
+            var firstProject = getFirstProjectOrCurrent(project);
+            var fakeEvent = createFakeActionEvent(firstProject);
+            ActionUtil.performActionDumbAwareWithCallbacks(mergeAllWindowsAction, fakeEvent);
+        });
+
         return null;
+    }
+
+    private boolean isIdeFullyInitialized(@NotNull Project project) {
+        var frames = WindowManager.getInstance().getAllProjectFrames();
+        var window = WindowManager.getInstance().getFrame(project);
+        var tabs = Foundation.invoke(MacUtil.getWindowFromJavaWindow(window), "tabbedWindows");
+        int tabCount = Foundation.invoke(tabs, "count").intValue();
+
+        return tabCount >= 2 && frames.length >= tabCount;
     }
 
     private Project getFirstProjectOrCurrent(Project project) {
@@ -36,9 +55,10 @@ class MacOsForceProjectTabs implements ProjectActivity {
     }
 
     private static AnActionEvent createFakeActionEvent(Project project) {
-        DataContext context = dataId ->
-                CONTEXT_COMPONENT.getName().equals(dataId) ? WindowManager.getInstance().getFrame(project) : null;
+        var window = WindowManager.getInstance().getFrame(project);
+        var dataContext = DataManager.getInstance().getDataContext(window);
 
-        return new AnActionEvent(null, context, "", new Presentation(), ActionManager.getInstance(), 0);
+        return new AnActionEvent(dataContext, new Presentation(), "",
+                ActionUiKind.MAIN_MENU, null, 0, ActionManager.getInstance());
     }
 }
